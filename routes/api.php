86a74c7d7218b85d0f918fbe61167389abc5cf59
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-08-03 08:44:17 +0300

    WIP

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/webhook-handler', function () {
+    // Run the deploy script
+    $process = new Process(['/bin/bash', '/home/digital07/round5-online-booking-with-doctor-api.digital-vision-solutions.com/deploy.sh']);
+    
+    try {
+        $process->mustRun(); // This will throw an exception if the command fails
+    } catch (ProcessFailedException $exception) {
+        return response('Deployment failed: ' . $exception->getMessage(), 500);
+    }
+
+    return response('Deployment completed successfully.', 200);
+});
