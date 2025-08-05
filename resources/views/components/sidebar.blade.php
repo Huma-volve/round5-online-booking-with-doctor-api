5d8f20313452f81d6d7e0db1b1f20b0b5017fea1
mariam <mariamemampp00@gmail.com>
2025-08-05 12:50:26 +0300

    initilaization & install auhtentication

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,31 @@
+<!-- Sidebar -->
+<nav class="sidebar sidebar-offcanvas bg-dark text-white" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link text-white" href="#">
+        <i class="mdi mdi-account-multiple menu-icon text-white"></i>
+        <span class="menu-title">Doctors</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-white" href="#">
+        <i class="mdi mdi-stethoscope menu-icon text-white"></i>
+        <span class="menu-title">Specialists</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-white" href="#">
+        <i class="mdi mdi-calendar-clock menu-icon text-white"></i>
+        <span class="menu-title">Bookings</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href = "/users"class="nav-link text-white" href="#">
+        <i class="mdi mdi-account-multiple menu-icon text-white"></i>
+        <span class="menu-title">Users</span>
+      </a>
+    </li>
+
+  </ul>
+</nav>
