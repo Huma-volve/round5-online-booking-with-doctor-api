083aaf1d30485b062419839e908ae09479ae6928
AhmedMagdy2210 <ahmed22102002@gmail.com>
2025-08-05 12:14:25 +0300

    Make the migrations,add FAQ's end-points and model,start the card and payment methdology and add spatie permission for the user

---
+++ app/Models/DoctorProfile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DoctorProfile extends Model {
+    protected $fillable = [
+        'user_id',
+        'specialty_id',
+        'hospital_id',
+        'about',
+        'price_per_hour',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function specialty() {
+        return $this->belongsTo(Specialty::class);
+    }
+    
+}
---
+++ app/Models/Hospital.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Location;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Hospital extends Model {
+    protected $fillable = [
+        'name',
+        'open_at',
+        'close_at',
+        'photo',
+    ];
+    public function location() {
+        return $this->morphTo(Location::class, 'addressable');
+    }
+}
