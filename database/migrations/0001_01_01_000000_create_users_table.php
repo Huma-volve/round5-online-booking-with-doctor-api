6c57709da64d7ebff781d05cbb06c518338ff5f9
AhmedMagdy2210 <ahmed22102002@gmail.com>
2025-08-06 21:17:02 +0300

    Finish tasks and modify

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,11 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-
+            $table->boolean('is_active')->default(true);
+            $table->string('phone');
+            $table->string('avatar')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->enum('type', ['admin', 'doctor', 'customer']);
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
