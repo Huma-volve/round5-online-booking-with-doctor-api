fde3f93d3933f816ff6503b43e257b616ed5edfe
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-08-07 21:49:33 +0300

    remove dublicated line

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
