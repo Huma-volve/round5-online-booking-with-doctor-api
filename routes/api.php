0fc52919b4868859e63001658eb57f3fbed0a8ec
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-08-27 20:10:53 +0300

    edit card controller namespace into api.php file

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\FaqController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\CardController;
+use App\Http\Controllers\Api\CardController;
 use App\Http\Controllers\API\NotificationController;
 use App\Http\Controllers\SpecialistController;
 use App\Http\Controllers\AppointmentController;
