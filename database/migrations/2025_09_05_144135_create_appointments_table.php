13fd14640bf6e9a060a070be2423fc995fc09fa4
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-09-05 17:46:20 +0300

    add appointments table

---
+++ database/migrations/2025_09_05_144135_create_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // patient
+            $table->foreignId('doctor_id')->constrained()->onDelete('cascade'); // doctor
+            $table->date('date'); // appointment date
+            $table->time('time'); // appointment time
+            $table->enum('status', ['pending', 'confirmed', 'canceled', 'completed'])->default('pending');
+            $table->boolean('is_paid')->default(false);
+            $table->string('payment_reference')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
